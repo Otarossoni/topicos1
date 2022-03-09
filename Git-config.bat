a1b127e6cc5e1c5485565c15dbcbdbf2a81db0df
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2022-03-08 21:59:11 -0300

    Adicionando novos arquivos .bat

---
+++ Git-config.bat
@@ -0,0 +1,5 @@
+rem git config --global user.name "Samuel Amarante"
+rem git config --global user.email "186218@upf.com"
+rem cd existing_folder
+rem git init --initial-branch=main
+rem git remote add origin https://github.com/SamuelAmarante/Topicos1
