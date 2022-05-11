ff91e85e7898cd39fa7a01fa3b269f2d06d02aaf
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2022-05-10 22:18:50 -0300

    Adicionando paginação e filtros

--- dev/app-intro2/src/pages/colaborador/ColaboradorList.js
+++ dev/app-intro2/src/pages/colaborador/ColaboradorList.js
@@ -1,58 +1,76 @@
+import React from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+
 const ColaboradorList = (props) => {
+  const paginatorLeft = (
+    <Button type="button" icon="pi pi-refresh" className="p-button-text" />
+  );
+  const paginatorRight = (
+    <Button type="button" icon="pi pi-cloud" className="p-button-text" />
+  );
+
   return (
     <div className="App">
-      <h4>Listagem de Colaboradores</h4>
-      <button
-        className="btn btn-primary btn-sm"
-        onClick={props.onClickAtualizar}
-      >
-        Atualizar Lista
-      </button>
-      <button className="btn btn-success btn-sm" onClick={props.inserir}>
-        Inserir
-      </button>
+      <br />
+      <h4>LISTAGEM DE COLABORADORES</h4>
+      <div style={{ margin: "1%" }}>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={props.onClickAtualizar}
+        >
+          Atualizar <i class="pi pi-refresh"></i>
+        </button>
+        <span> </span>
+        <button className="btn btn-success btn-sm" onClick={props.inserir}>
+          Adicionar <i class="pi pi-plus"></i>
+        </button>
+      </div>
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Senha</th>
-            <th>Operações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.colaboradores.length > 0 ? (
-            props.colaboradores.map((o, index) => (
-              <tr key={index}>
-                <td>{o._id}</td>
-                <td>{o.nome}</td>
-                <td>{o.email}</td>
-                <td>{o.senha}</td>
-                <td>
+      <div className="card">
+        <DataTable
+          value={props.colaboradores}
+          responsiveLayout="scroll"
+          paginator
+          paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+          currentPageReportTemplate="Mostrando de {first} até {last} de {totalRecords}"
+          rows={10}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          paginatorLeft={paginatorLeft}
+          paginatorRight={paginatorRight}
+          selectionMode="single"
+          selection={props.colaborador}
+          onSelectionChange={(e) => props.setColaborador(e.value)}
+        >
+          <Column field="_id" header="Id" sortable></Column>
+          <Column field="nome" header="Nome" sortable filter></Column>
+          <Column field="email" header="E-mail" sortable filter></Column>
+          <Column field="senha" header="Senha" sortable></Column>
+          <Column
+            header="Operações"
+            body={(row) => {
+              return (
+                <>
                   <button
-                    onClick={() => props.editar(o._id)}
+                    onClick={() => props.editar(row._id)}
                     className="btn btn-warning btn-sm"
                   >
-                    Editar
+                    <i class="pi pi-pencil"></i>
                   </button>
+                  <span> </span>
                   <button
-                    onClick={() => props.excluir(o._id)}
+                    onClick={() => props.excluir(row._id)}
                     className="btn btn-danger btn-sm"
                   >
-                    Excluir
+                    <i class="pi pi-trash"></i>
                   </button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={3}>Nenhum colaborador!.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                </>
+              );
+            }}
+          ></Column>
+        </DataTable>
+      </div>
     </div>
   );
 };
